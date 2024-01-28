7333f5a33b02ab718714b10d99fe450eb45c5be9
giulgu17 <66566897+giulgu17@users.noreply.github.com>
2024-01-28 19:02:02 +0100

    Initial commit

---
+++ game.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tris</title>
+</head>
+<body>
+    <section id="game">
+        <div id="tris">
+            <div id="1" class="box"></div>
+            <div id="2" class="box"></div>
+            <div id="3" class="box"></div>
+            <div id="4" class="box"></div>
+            <div id="5" class="box"></div>
+            <div id="6" class="box"></div>
+            <div id="7" class="box"></div>
+            <div id="8" class="box"></div>
+            <div id="9" class="box"></div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"/>
+    <title>Tris</title>
+</head>
+<body>
+    <h1>Tris</h1>
+    <form action="game.php" method="post" id="home">
+        <input type="input" id="inputnick" name="scelta" placeholder="Nickname"><br>
+        <input type="submit" id="buttonnick" value="Entra">
+    </form>
+</body>
+</html>
