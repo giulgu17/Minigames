5a56b36006518867db5a8e80781e72d5ce66b352
giulgu17 <66566897+giulgu17@users.noreply.github.com>
2024-05-11 21:47:20 +0200

    .

--- Battleship/public/javascripts/home.js
+++ Battleship/public/javascripts/home.js
@@ -1,4 +1,5 @@
 var rows = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+var selected;
 var selectMode = false;
 
 function ready() {
@@ -17,8 +18,10 @@ function ready() {
 
 }
 
+
+
 function select() {
-    var selected = this;
+    selected = this;
     selectMode = true;
     selected.classList.toggle("selected");
     selected.style.backgroundColor = selected.classList.contains("selected") ? "lime" : "yellow";
@@ -27,21 +30,33 @@ function select() {
             var square = document.getElementById("s" + (i * 10 + j));
             square.style.cursor = "pointer";
             //TODO: show preview onHover (check Conway's Game of Life project)
-            square.addEventListener("click", place());
+            square.addEventListener("click", place);
         }
     }
 }
 
 function place(){
     var clicked = this;
-    document.getElementById(this.id).classList.add("ship");
+    document.getElementById(clicked.id).classList.add("ship");
     //TODO: add ship types and direction
-    //TODO: check if a ship is already there or next to it (check minesweeper project)
-    for(i=0;i<10;i++){
+    //TODO: check if a ship is already there or next to it
+    //Based on the direction and the type of ship, check the adjacent squares somehow
 
+
+    for(var i = 0; i < 10; i++){        //PLACEHOLDER VALUES, CHANGE THESE!!!!
+        checkedSquare = document.getElementById("s" + (i * 10 + 1));
+        if(!checkedSquare.classList.contains("ship")){
+            checkedSquare.classList.add("ship");
+        } else {
+            return;
+        }
     }
+    
     //TODO: then remove the outside ship
+    selected.remove();
 }
+
+
 
 function join() {
     var hidden = document.createElement("input");
